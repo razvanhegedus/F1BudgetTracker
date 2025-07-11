db40d233fb2ec56874cd4b4a1e394dbbda3e9a2e
razvanhegedus <hegedusrazvan04@gmail.com>
2025-07-11 13:54:20 -0700

    created the project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.expensetracker.f1expensetracker {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.expensetracker.f1expensetracker to javafx.fxml;
+    exports com.expensetracker.f1expensetracker;
+}
